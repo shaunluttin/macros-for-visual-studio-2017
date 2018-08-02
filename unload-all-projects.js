fd2ae53cc01147073f45d380f27efe9dcd4a1c03
Shaun Luttin <shaunluttin@gmail.com>
2018-08-02 11:48:28 -0700

    Start on unload-all-projects.js

---
+++ unload-all-projects.js
@@ -0,0 +1,22 @@
+/// <reference path="C:\Users\bigfo\AppData\Local\Microsoft\VisualStudio\15.0_26cdf119\Macros\dte.js" />
+
+Macro.InsertText('foo');
+
+var sln = dte.Solution;
+var slnName = dte.Solution.FullName;
+Macro.InsertText('sln' + slnName);
+
+//var objs = dte.Windows;
+//var objs = dte.Documents;
+var objs = dte.Solution.Projects;
+for (var i = 1; i <= objs.Count; i++) {
+    var o = objs.Item(i);
+
+    dte.ActiveDocument.Selection.NewLine();
+    Macro.InsertText('obj' + i);
+
+    dte.ActiveDocument.Selection.NewLine();
+    Macro.InsertText(o.Name);
+}
+
+Macro.InsertText('baz');
